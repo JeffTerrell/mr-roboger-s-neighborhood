f98da665d8a40309630deab428255e6bb9ca9876
Jeff Terrell <jeffaterrell@gmail.com>
2021-10-29 18:52:51 -0400

    add arrayFromInput function to numberIdentifierLoop function

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,11 @@ function arrayFromInput (number) {
   arrayUserInput = Array.from({length: number+1}, (v, i) => i);
   return arrayUserInput;
 }
-arrayFromInput(userInput);
-console.log
+
 
 function numberIdentifierLoop (number) {
-  for (let i = 0; i < number.length; i++) {
+  arrayFromInput(number);
+  for (let i = 0; i < arrayUserInput.length; i++) {
     if ((i+'').indexOf('3') > -1) {
       userOutput.push("Won't you be my neighbor?");
     } else if ((i+'').indexOf('2') > -1) {
