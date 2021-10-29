a80bce39ae63e93b94a8da89d4d3f7d7e60d5d72
Jeff Terrell <jeffaterrell@gmail.com>
2021-10-29 12:34:45 -0400

    WIP: Working on logic to recognize all numbers containing 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 // Business Logic
 
+// work in progress to identify if number contains 1 not just equals 1
 function numberIdentifier (number) {
-  if (number = 1) {
+  if (number === 1) {
     return "beep"
   }
 }
